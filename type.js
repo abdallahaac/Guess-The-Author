ca05ee755e6d3a1417d44178f29ae4727b121acb
Abdallah <abdallahabouchahine@cmail.carleton.ca>
2022-03-30 00:13:19 -0400

    Fixed typo

--- type.js
+++ type.js
@@ -1,3 +1,7 @@
+// setTimeout(function () {
+//   document.getElementById("submit").click();
+// }, 100);
+
 if (window.history.replaceState) {
   window.history.replaceState(null, null, window.location.href);
 }
